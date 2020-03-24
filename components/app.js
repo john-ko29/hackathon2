3084504975ea09155f0aa181be6b298b2e4e4a10
John Ko <jko121790@gmail.com>
2020-03-24 15:46:36 -0700

    second ajax complete

--- components/app.js
+++ components/app.js
@@ -5,6 +5,13 @@ class App {
     this.handleGetMemesSuccess = this.handleGetMemesSuccess.bind(this);
     this.handleGetMemesError = this.handleGetMemesError.bind(this);
     this.updateMemes = this.updateMemes.bind(this);
+    this.weatherAPI = "2bfe50c866295a179e87fa1e712f39bc";
+    this.gifAPI = "MhQsunQ2WuoHo7CFjWWy0YrOR5aVrP0u"
+    this.getWeather = this.getWeather.bind(this);
+    this.handleGetWeatherSuccess = this.handleGetWeatherSuccess.bind(this);
+    this.handleGetWeatherError = this.handleGetWeatherError.bind(this);
+    this.memesData = null;
+    this.weatherData = null;
   }
 
   getMemes() {
@@ -26,14 +33,14 @@ class App {
 
   updateMemes(memes) {
     this.mainElement.innerHTML = "";
-    var memesData = memes.data.memes;
-    console.log(memesData);
-    for (var i = 0; i < memesData.length; i++) {
+    this.memesData = memes.data.memes;
+    console.log(this.memesData);
+    for (var i = 0; i < this.memesData.length; i++) {
       var imgElement = document.createElement("img")
-      imgElement.setAttribute("src", memesData[i].url);
-      imgElement.classList.add("col-3");
-      imgElement.setAttribute("id", memesData[i].id);
-      imgElement.setAttribute("name", memesData[i].name);
+      imgElement.setAttribute("src", this.memesData[i].url);
+      imgElement.classList.add("col-4");
+      imgElement.setAttribute("id", this.memesData[i].id);
+      imgElement.setAttribute("name", this.memesData[i].name);
       imgElement.setAttribute("arrayIndex", i);
 
       this.mainElement.appendChild(imgElement);
@@ -42,5 +49,26 @@ class App {
 
   start() {
       this.getMemes()
+  }
+
+  getWeather() {
+    var url = "http://api.openweathermap.org/data/2.5/group?id=5359777,5368361,5128581,3582383,5475433&units=metric&appid=" + this.weatherAPI;
+    $.ajax({
+      method: "GET",
+      url: url,
+      success: this.handleGetWeatherSuccess,
+      error: this.handleGetWeatherError
+    })
+  }
+
+  handleGetWeatherSuccess(weather) {
+    console.log(weather);
+    for(var i = 0; i < weather.length; i++) {
+      console.log(weather[i].weather.main)
+    }
+  }
+
+  handleGetWeatherError(error) {
+    console.error(error);
   }
 }
