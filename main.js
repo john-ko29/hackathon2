4c7f29682a926c8ddfa006ece5a837d888c26bc9
John Ko <jko121790@gmail.com>
2020-05-19 17:29:12 -0700

    hide help modal function

--- main.js
+++ main.js
@@ -5,7 +5,9 @@ var optionalElement = document.getElementById("formOptional");
 var cityForecastElement = document.getElementById("formCity");
 var invalidModalElement = document.getElementById("invalidModal");
 var invalidOkayElement = document.getElementById("invalidOkay");
+var helpModalElement = document.getElementById("helpModal");
+var helpConfirmElement = document.getElementById("helpConfirm");
 
 var forecast = new Forecast(mainElement, formElement, optionalElement, cityFormElement, cityForecastElement, invalidModalElement);
-var app = new App(mainElement, forecast, invalidOkayElement, invalidModalElement);
+var app = new App(mainElement, forecast, invalidOkayElement, invalidModalElement, helpConfirmElement, helpModalElement);
 app.start();
